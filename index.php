9743a67cf4c8618a1831ac807b205bf4131cc508
lilia <lilianom59@outlook.fr>
2023-01-16 13:33:02 +0100

    form

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo
+"<form method='post' action='form.php'>
+    <label>Username</label>
+    <input type='text' name='username' id='idd-username'>
+    <input type='submit' value='Envoyer' name='submit'>
+</form>";
